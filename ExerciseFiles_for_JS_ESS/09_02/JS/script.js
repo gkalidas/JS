aeb29d241cc2f34850a0ac7f6a6429c27e6b0f96
Ganesh Londhe <ganeshlondhe2020@gmail.com>
2019-06-10 13:10:49 +0530

    setting external links to blank

---
+++ ExerciseFiles_for_JS_ESS/09_02/JS/script.js
@@ -0,0 +1,9 @@
+// only links starts with http/s
+var extLinks = document.querySelectorAll('a[href^="http"]');
+console.log(extLinks);
+for(var i =0 ; i<extLinks.length;i++){
+  if(!extLinks[i].hasAttribute("target")){
+    extLinks[i].setAttribute("target","_blank");
+  }
+  
+}
