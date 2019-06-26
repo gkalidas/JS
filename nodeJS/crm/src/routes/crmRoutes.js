ec872665c1d2781b1e6e7f60da931914bf650b26
Ganesh Londhe <ganeshlondhe2020@gmail.com>
2019-06-26 15:46:53 +0530

    inserting values into DB and retrieving the same with mongoDB

--- nodeJS/crm/src/routes/crmRoutes.js
+++ nodeJS/crm/src/routes/crmRoutes.js
@@ -1,18 +1,18 @@
+import { addNewContact, getContacts } from "../controllers/crmController";
+
+
 const routes = (app) => {
     app.route('/contact')
-    .get((req, res) =>{
+    .get((req, res,next) =>{
         //middleware
         console.log(`Request from: ${req.originalUrl}`)
         console.log(`Request type: ${req.method}`)
         next();
 
-    },(req,res,next) =>{
-        res.send('GET request successfull!!!')
-    })
+    },getContacts)
     
-
-    .post((req, res) =>
-    res.send('POST request successfull!!!'));
+//post endpoint
+    .post(addNewContact);
 
     app.route('/contact/:contactId')
     .put((req, res) =>
